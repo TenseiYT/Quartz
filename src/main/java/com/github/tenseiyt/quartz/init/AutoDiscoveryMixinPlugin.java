8824ef90034b7013f42d3b338ec0fd8e1ea53510
GitHub Action <action@github.com>
2024-07-10 05:03:22 +0000

    Initialize template repository proper
    Set package name to com.github.tenseiyt.quartz
    Set project name to Quartz
    Set modid to quartz

--- src/main/java/com/github/tenseiyt/quartz/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/tenseiyt/quartz/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.tenseiyt.quartz.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
